d73d3f9bbc26a00ffe93cc9ce31f727ed972e156
naoki asami <n-asami@daiwa-sc.co.jp>
2023-11-18 16:27:24 +0900

    Todo 登録処理

--- app.vue
+++ app.vue
@@ -1,13 +1,28 @@
 <template>
-  <v-row class="ma-5">
-    <v-list>
-      <v-list-item v-for="todo in todoList" :key="todo.id" :title="todo.title" :subtitle="todo.detail" />
-    </v-list>
-  </v-row>
+  <div class="ma-10">
+    <v-row>
+      <v-col><v-text-field v-model="todoTitle" /></v-col>
+      <v-col><v-text-field v-model="todoDetail" /></v-col>
+      <v-col><v-btn @click="addTodo" icon="mdi-plus" /></v-col>
+    </v-row>
+
+    <v-row>
+      <v-list>
+        <v-list-item v-for="todo in todoList" :key="todo.id" :title="todo.title" :subtitle="todo.detail" />
+      </v-list>
+    </v-row>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+
+const todoTitle = ref();
+const todoDetail = ref();
+
+const addTodo = function() {
+  todoList.value.push({id: 9, title: todoTitle.value, detail: todoDetail.value, done: false  })
+}
 
 const todoList = ref([
   { id:1, title:"Todo1", detail: "TodoTodoTodo1", done: false },
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,9 +1,13 @@
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components'
+import '@mdi/font/css/materialdesignicons.css'
 
 export default defineNuxtPlugin( nuxtApp => {
   const vuetify = createVuetify({
-    components
+    components,
+    icons: {
+      defaultSet: "mdi",
+    },
   });
 
   nuxtApp.vueApp.use( vuetify );
